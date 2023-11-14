58fe4e4597088f5f48e9a50d6227d25037f3846c
Stéphane Demonchaux <demonchaux.stephane@gmail.com>
2023-11-14 10:13:16 +0100

    fix #8

--- src/CertificateX509.php
+++ src/CertificateX509.php
@@ -6,9 +6,12 @@ namespace Cube43\Component\Ebics;
 
 use phpseclib\File\X509;
 use RuntimeException;
+use Throwable;
 
 use function array_map;
 use function array_shift;
+use function base64_encode;
+use function chunk_split;
 use function hash;
 use function implode;
 use function is_array;
@@ -38,7 +41,13 @@ class CertificateX509
 
     public function fingerprint(): string
     {
-        $digest  = strtoupper(openssl_x509_fingerprint($this->value, 'sha256'));
+        try {
+            $digest = strtoupper(openssl_x509_fingerprint($this->value, 'sha256'));
+        } catch (Throwable) {
+            $under  = "-----BEGIN CERTIFICATE-----\r\n" . chunk_split(base64_encode($this->value), 64) . '-----END CERTIFICATE-----';
+            $digest = strtoupper(openssl_x509_fingerprint($under, 'sha256'));
+        }
+
         $digests = str_split($digest, 16);
         $digests = array_map(static function ($digest) {
             return wordwrap($digest, 2, ' ', true);
