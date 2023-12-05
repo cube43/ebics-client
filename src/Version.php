dc20d03bf3e21a102681e636ee1bb582dc9d515b
Sébastien Lamard <sebastien.lamard.pro@gmail.com>
2023-12-05 14:53:29 +0100

    fix #195 : Add Svirin handled versions

--- src/Version.php
+++ src/Version.php
@@ -9,6 +9,9 @@ class Version
     private const V24 = 'H003';
     private const V25 = 'H004';
     private const V30 = 'H005';
+    private const V24_SVIRIN = 'VERSION_24';
+    private const V25_SVIRIN = 'VERSION_25';
+    private const V30_SVIRIN = 'VERSION_30';
 
     private function __construct(private readonly string $value)
     {
@@ -27,6 +30,21 @@ class Version
     public static function v30(): self
     {
         return new self(self::V30);
+    }
+
+    public static function v24Svirin(): self
+    {
+        return new self(self::V24_SVIRIN);
+    }
+
+    public static function v25Svirin(): self
+    {
+        return new self(self::V25_SVIRIN);
+    }
+
+    public static function v30Svirin(): self
+    {
+        return new self(self::V30_SVIRIN);
     }
 
     public function is(Version $version): bool
